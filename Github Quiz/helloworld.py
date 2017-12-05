84387c69b6eb488e751be1035cadab916b6d7697
cxiao18 <cxiao18@choate.edu>
2017-12-05 08:17:28 -0500

    Interactive Version

--- Github Quiz/helloworld.py
+++ Github Quiz/helloworld.py
@@ -1,15 +1,15 @@
-#Hello, world!
+#import sys
 
-import sys
 
-print('Hello, ', end= '')
-print(sys.argv[1], end=', ')
-print(sys.argv[2], end=' and ')
-print(sys.argv[3], end='! ')
-print('Welcome.')
+response = input('What is your name? ')
+print('Hello, ' +response + '!')
+
+#print('Hello, ', end= '')
+#print(sys.argv[1], end=', ')
+#print(sys.argv[2], end=' and ')
+#print(sys.argv[3], end='! ')
+#print('Welcome.')
+
 
 #Respond here: The end= inserts whatever is in the quotes at the end of the line after the print command runs
 #To run this code: Type "python3 helloname.py " followed by the 3 names with spaces between them
-
-response = input('What is your favorite food? ') 
-print ('Mmm!')
