ff83521f0fda68cd47721441c28d8d1235c78da0
cxiao18 <cxiao18@choate.edu>
2017-12-07 21:49:40 -0500

    a

---
+++ messagingsystem.py
@@ -0,0 +1,9 @@
+#Goals: practice github and classes
+
+#Messaging system: choose 3 outdated forms of messaging, write a class representing each, write a runner/main class, write a class to file
+
+#Runner class will say: welcome to our messaging system, what would you like to say?
+
+#File class will file the message, so it is ready to send off
+
+
